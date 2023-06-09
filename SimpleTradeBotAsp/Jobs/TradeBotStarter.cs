e882d5fd28259b2281894690ff9a86261988a6df
Алексей <tolexan@live.ru>
2023-06-09 15:57:26 +0400

    Первая рабочая asp.net core версия

---
+++ SimpleTradeBotAsp/Jobs/TradeBotStarter.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System.Net.Mail;
+using System.Net;
+using SimpleTradeBot;
+using System.Threading.Tasks;
+
+namespace SimpleTradeBotAsp.Jobs
+{
+    public class TradeBotStarter: IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            TradeBot tb = new TradeBot();
+            await Task.Run(() => tb.TradeStep());
+        }
+    }
+}
